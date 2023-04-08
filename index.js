cf13b43127a818f7d887e103c0c72aadc78cc5e7
Joshua Cavell <xclusive36@gmail.com>
2023-04-08 19:07:17 -0400

    Application can now add notes

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const express = require("express"); // require express
 const path = require("path"); // require path
-
-var fs = require("fs"); // require file system
+const fs = require("fs"); // require fs
 
 const app = express(); // create express app
 const port = 3000; // set port
 
 app.use(express.static("public")); // set static folder
+app.use(express.json()); // parse JSON
 
-app.get("/api/notes", function (req, res) { // get notes from db.json
+app.get("/api/notes", function (req, res) {
+  // get notes from db.json
   res.sendFile(path.join(__dirname, "./db/db.json")); // send notes to client
 });
 
-app.get("/notes", function (req, res) { // get notes path from client side and send notes.html first so wildcard doesn't take over
+app.post("/api/notes", function (req, res) {
+  // get post request from client, add note to db.json
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    // read db.json
+    if (err) throw err; // throw error if error
+    const notes = JSON.parse(data); // parse db.json
+    notes.push(req.body); // push new note to notes array
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      // write new notes array to db.json
+      if (err) throw err; // throw error if error
+      res.status(200).json({success: 'Note successfully saved!'});
+    });
+  });
+});
+
+app.get("/notes", function (req, res) {
+  // get notes path from client side and send notes.html first so wildcard doesn't take over
   res.sendFile(path.join(__dirname, "./public/notes.html")); // send notes.html to client
 });
 
-app.get("*", function (req, res) { // get all other paths from client side and send index.html
+app.get("*", function (req, res) {
+  // get all other paths from client side and send index.html
   res.sendFile(path.join(__dirname, "./public/index.html")); // send index.html to client
 });
 
-app.listen(port, () => { // listen on port
+app.listen(port, () => {
+  // listen on port
   console.log(`Example app listening on port ${port}`); // log port
 });
